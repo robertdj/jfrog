49c0f84c5c7b03cfc901803de2dccf9166a8e916
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-05-16 21:48:45 +0200

    ✅ Move test package into test

--- tests/testthat/test-upload_package.R
+++ tests/testthat/test-upload_package.R
@@ -1,7 +1,7 @@
-source_package <- create_empty_package("foo", "0.0.1", binary = FALSE, quiet = TRUE)
-withr::defer(unlink(source_package))
-
 test_that("Error trying to upload w/o authentication", {
+    source_package <- create_empty_package("foo", "0.0.1", binary = FALSE, quiet = TRUE)
+    withr::defer(unlink(source_package))
+
     expect_error(
         upload_package(source_package, "jfrog_url", api_key = NA_character_),
         regexp = "No authentication credentials provided"

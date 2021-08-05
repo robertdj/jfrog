d5d528eef2dc0b6f8eef85da32f2e96238e6dd18
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-08-05 22:07:58 +0200

    ✅ Check all combinations of authentication

--- tests/testthat/test-upload_package.R
+++ tests/testthat/test-upload_package.R
@@ -3,7 +3,17 @@ test_that("Error trying to upload w/o authentication", {
     withr::defer(unlink(source_package))
 
     expect_error(
+        upload_package(source_package, "jfrog_url", access_token = NA_character_),
+        regexp = "No authentication credentials provided"
+    )
+
+    expect_error(
         upload_package(source_package, "jfrog_url", api_key = NA_character_),
+        regexp = "No authentication credentials provided"
+    )
+
+    expect_error(
+        upload_package(source_package, "jfrog_url", api_key = NA_character_, access_token = NA_character_),
         regexp = "No authentication credentials provided"
     )
 })

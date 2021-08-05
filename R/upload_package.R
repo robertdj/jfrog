b239b4009e76833fc61fe9ad9632e70d51df9e35
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-08-05 22:11:54 +0200

    🎨 Less indentation

--- R/upload_package.R
+++ R/upload_package.R
@@ -43,19 +43,17 @@ is_valid_key <- function(x)
 make_cran_suffix <- function(package_archive)
 {
     if (pkg.peek::package_ext(package_archive) == "tar.gz") {
-        cran_suffix <- "sources"
-    } else {
-        r_version <- pkg.peek::get_r_version(package_archive)
-        version_for_cran <- paste0(r_version$major, ".", r_version$minor)
-
-        if (pkg.peek::package_ext(package_archive) == "zip") {
-            os <- "windows"
-        } else if (pkg.peek::package_ext(package_archive) == "tgz") {
-            os <- "macosx"
-        }
-
-        cran_suffix <- paste0("binaries?distribution=", os, "&rVersion=", version_for_cran)
+        return("sources")
     }
 
-    return(cran_suffix)
+    r_version <- pkg.peek::get_r_version(package_archive)
+    version_for_cran <- paste0(r_version$major, ".", r_version$minor)
+
+    if (pkg.peek::package_ext(package_archive) == "zip") {
+        os <- "windows"
+    } else if (pkg.peek::package_ext(package_archive) == "tgz") {
+        os <- "macosx"
+    }
+
+    paste0("binaries?distribution=", os, "&rVersion=", version_for_cran)
 }

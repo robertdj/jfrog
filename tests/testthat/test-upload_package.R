8e66e48998403009a3dd375006b58e1844cb31a8
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-08-06 22:39:32 +0200

    ✅ Create test packages in setup file

--- tests/testthat/test-upload_package.R
+++ tests/testthat/test-upload_package.R
@@ -1,7 +1,4 @@
 test_that("Error trying to upload w/o authentication", {
-    source_package <- create_empty_package("foo", "0.0.1", binary = FALSE, quiet = TRUE)
-    withr::defer(unlink(source_package))
-
     expect_error(
         upload_package(source_package, "jfrog_url", access_token = NA_character_),
         regexp = "No authentication credentials provided"
@@ -22,8 +19,14 @@ test_that("Error trying to upload w/o authentication", {
 httptest::use_mock_api()
 
 test_that("Upload source package", {
-    source_package_archive <- pkgbuild::build(binary = FALSE, quiet = TRUE)
-    response <- jfrog::upload_package(source_package_archive, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
+    response <- jfrog::upload_package(source_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
+
+    expect_equal(httr::status_code(response), 201L)
+})
+
+
+test_that("Upload binary package", {
+    response <- jfrog::upload_package(binary_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
 
     expect_equal(httr::status_code(response), 201L)
 })

8ea2b2f7d940426eb2afa8477e418f7257622bc8
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-08-27 08:32:12 +0200

    ✅ Remove tests

--- tests/testthat/test-upload_package.R
+++ tests/testthat/test-upload_package.R
@@ -1,15 +1,5 @@
 test_that("Error trying to upload w/o authentication", {
     expect_error(
-        upload_package(source_package, "jfrog_url", access_token = NA_character_),
-        regexp = "No authentication credentials provided"
-    )
-
-    expect_error(
-        upload_package(source_package, "jfrog_url", api_key = NA_character_),
-        regexp = "No authentication credentials provided"
-    )
-
-    expect_error(
         upload_package(source_package, "jfrog_url", api_key = NA_character_, access_token = NA_character_),
         regexp = "No authentication credentials provided"
     )
@@ -19,14 +9,14 @@ test_that("Error trying to upload w/o authentication", {
 httptest::use_mock_api()
 
 test_that("Upload source package", {
-    response <- jfrog::upload_package(source_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
+    response <- upload_package(source_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
 
     expect_equal(httr::status_code(response), 201L)
 })
 
 
 test_that("Upload binary package", {
-    response <- jfrog::upload_package(binary_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
+    response <- upload_package(binary_package, jfrog_url = "https://HOSTNAME/artifactory/api/cran/cran-local", api_key = "api_key")
 
     expect_equal(httr::status_code(response), 201L)
 })
